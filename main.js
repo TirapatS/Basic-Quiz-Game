42d56b036858e9cf3eef0762d730c4400eb1aca0
Tirapat <tirapat.s02@gmail.com>
2021-11-16 08:59:51 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+const slider_img = document.querySelector('.profile-img')
+const profileimages = []
+
 const student = {
     fullName: "",
     pfp_img: "",
+}
+
+function setImg() {
+    return slider_img.setAttribute('src', 'profileimages/ ' + profileimages[i])
 }
